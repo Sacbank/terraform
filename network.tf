fa491ff1d560ea5061f84beae7e9b6952a12920e
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-03-21 14:38:23 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -17,8 +17,8 @@ resource "aws_internet_gateway" "my_igw" {
 # Create a public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"  # Change to your desired CIDR block
-  availability_zone = "us-east-1a"   # Change to your desired availability zone
+  cidr_block        = "192.168.0.0/22"
+  availability_zone = "us-east-1a"  
 
   map_public_ip_on_launch = true
   tags = {
@@ -29,8 +29,8 @@ resource "aws_subnet" "public_subnet" {
 # Create a private subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.0/24"  # Change to your desired CIDR block
-  availability_zone = "us-east-1a"   # Change to your desired availability zone
+  cidr_block        = "192.168.4.0/22"
+  availability_zone = "us-east-1a"  
   tags = {
     Name = "Priv-Subnet"
   }   
