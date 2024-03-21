e88fbf38c2afa45e91045830799312698d59a911
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-03-21 12:25:46 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-${var.latest["ubuntu"]}-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+variable "latest" {
+  type = map(any)
+  default = {
+    "ubuntu"  = "22.04"
+  }
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  default = "virginia"
+}
