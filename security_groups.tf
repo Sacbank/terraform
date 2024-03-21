de109f48c91aa270af549491753a83bc054f4eae
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-03-21 14:37:51 +0530

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -1,6 +1,32 @@
-# Create a security group for the EC2 instance
-resource "aws_security_group" "instance_sg" {
-  name        = "instance_sg"
+# Create a security group for the Private EC2 instance
+resource "aws_security_group" "Priv_instance_sg" {
+  name        = "Priv_instance_sg"
+  description = "Security group for the EC2 instance"
+
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_instance.public_instance.private_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Private-Instance-SG"
+  }   
+}
+
+# Create a security group for the Public EC2 instance
+resource "aws_security_group" "Pub_instance_sg" {
+  name        = "Pub_instance_sg"
   description = "Security group for the EC2 instance"
 
   vpc_id = aws_vpc.my_vpc.id
@@ -12,14 +38,6 @@ resource "aws_security_group" "instance_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # # Allow HTTP access for example
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -28,6 +46,6 @@ resource "aws_security_group" "instance_sg" {
   }
 
   tags = {
-    Name = "Instance-SG"
+    Name = "Public-Instance-SG"
   }   
 }
