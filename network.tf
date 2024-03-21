f77eb298b8e7a6facdbc9d3c7c9579a1c7c00dfc
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-03-21 12:27:02 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,70 @@
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.cidr_block
+  tags = {
+    Name = "My_VPC"
+  }  
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "IGW"
+  }  
+}
+
+# Create a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"  # Change to your desired CIDR block
+  availability_zone = "us-east-1a"   # Change to your desired availability zone
+
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Pub-Subnet"
+  }  
+}
+
+# Create a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"  # Change to your desired CIDR block
+  availability_zone = "us-east-1a"   # Change to your desired availability zone
+  tags = {
+    Name = "Priv-Subnet"
+  }   
+}
+
+# Create a public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "Pub-RT"
+  }   
+}
+
+# Associate public subnet with the public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create a private route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "Priv-RT"
+  }   
+}
+
+# Associate private subnet with the private route table
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
