7d8c5152428e2127931474fa8f1731d88f854407
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-03-27 14:14:22 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -22,6 +22,26 @@ resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_policy" "s3_get_object_policy" {
+  name        = "s3_get_object_policy"
+  description = "Policy for S3 getObject operation"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "s3:GetObject",
+        Resource = "*",
+      },
+    ],
+  })
+}
+ 
+resource "aws_iam_role_policy_attachment" "S3_attachment" {
+  role       = aws_iam_role.example_role.name
+  policy_arn = aws_iam_policy.s3_get_object_policy.arn
+}
+
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "instance_profile"
   role = aws_iam_role.example_role.name
